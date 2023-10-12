a851f090eda182d966baa4705c55972ae7c1133e
Paul David Tan <cryxuz@gmail.com>
2023-10-12 19:44:39 +1300

    installed types/express

--- server/server.ts
+++ server/server.ts
@@ -7,7 +7,7 @@ const server = express()
 
 server.use(express.json())
 
-server.use('/api/v1/', )
+server.use('/api/v1/booking', )
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
